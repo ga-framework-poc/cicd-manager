5af4ab0cba577a3ec8583631864da168a92526c7
Evan A Slatis <eslatis@redhat.com>
2022-05-06 12:15:03 -0500

    programming error

--- run-step/steps/infra/setup-environment.sh
+++ run-step/steps/infra/setup-environment.sh
@@ -10,7 +10,6 @@ echo "CICD_ONBOARDING=${CICD_ONBOARDING}" >> ${GITHUB_ENV}
 echo "CICD_MANAGER=${CICD_MANAGER}" >> ${GITHUB_ENV}
 
 echo "SYSTEM_NAME=${{ github.event.inputs.system-name }}" >> ${GITHUB_ENV}
-echo "TEAM_NAME=${{ github.event.inputs.system-name }}" >> ${GITHUB_ENV}
 
 echo "CICD_REPO_ONBOARDING_DIR=${GITHUB_WORKSPACE}/${CICD_ONBOARDING}" >> ${GITHUB_ENV}
 echo "CICD_REPO_MGR_DIR=${GITHUB_WORKSPACE}/${CICD_MANAGER}" >> ${GITHUB_ENV}
