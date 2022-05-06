bfa6e4c1e7ef3e76cdcd33fc15799928e32a3e50
Evan A Slatis <eslatis@redhat.com>
2022-05-06 18:25:09 -0500

    testing

--- run-step/steps/infra/parse-system-def.sh
+++ run-step/steps/infra/parse-system-def.sh
@@ -6,13 +6,12 @@ SYSTEM_DEFS_FILE=$(ls ${SYSTEM_DEFS_DIR}/${SYSTEM_NAME}.y*)
 echo "SYSTEM_DEFS_FILE=${SYSTEM_DEFS_FILE}" >> ${GITHUB_ENV}
 
 TEAM_NAME=$(yq '.team' "${SYSTEM_DEFS_FILE}")
-echo "TEAM_NAME: ${TEAM_NAME}"
 echo "TEAM_NAME=${TEAM_NAME}" >> ${GITHUB_ENV}
 
 DEV_BRANCH=$(yq '.branch' "${SYSTEM_DEFS_FILE}")
 echo "DEV_BRANCH=${DEV_BRANCH}" >> ${GITHUB_ENV}
 
-REPO_NAMES=$(yq '[.organization + "/" + .components[].repo]' "${SYSTEM_DEFS_FILE}" -o json)
+REPO_NAMES=$(yq '[.organization + "/" + .components[].repo]' "${SYSTEM_DEFS_FILE}" -o json -I=0)
 echo "REPO_NAMES=${REPO_NAMES}" >> ${GITHUB_ENV}
 
 DEV_ENVIRONMENT=$(yq '.dev.name' "${SYSTEM_DEFS_FILE}")
@@ -21,10 +20,10 @@ echo "DEV_ENVIRONMENT=${DEV_ENVIRONMENT}" >> ${GITHUB_ENV}
 DEV_RESOURCEQUOTA=$(yq '.dev.size' "${SYSTEM_DEFS_FILE}")
 echo "DEV_RESOURCEQUOTA=${DEV_RESOURCEQUOTA}" >> ${GITHUB_ENV}
 
-TEST_ENVIRONMENTS=$(yq '.test-environments | keys' "${SYSTEM_DEFS_FILE}" -o json)
+TEST_ENVIRONMENTS=$(yq '.test-environments | keys' "${SYSTEM_DEFS_FILE}" -o json -I=0)
 echo "TEST_ENVIRONMENTS=${TEST_ENVIRONMENTS}" >> ${GITHUB_ENV}
 
-TEST_RESOURCEQUOTAS=$(yq '[.test-environments.*]' "${SYSTEM_DEFS_FILE}" -o json)
+TEST_RESOURCEQUOTAS=$(yq '[.test-environments.*]' "${SYSTEM_DEFS_FILE}" -o json -I=0)
 echo "TEST_RESOURCEQUOTAS=${TEST_RESOURCEQUOTAS}" >> ${GITHUB_ENV}
 
 echo "We made it!!!"
