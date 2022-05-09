022e8190ac3e403edf6961fa6c1028e253e335f4
Evan A Slatis <eslatis@redhat.com>
2022-05-09 13:52:38 -0500

    programming error

--- run-step/steps/java11/build.sh
+++ run-step/steps/java11/build.sh
@@ -3,7 +3,7 @@
 export JAVA_TOOL_OPTIONS=
 if [[ -f $(dirname ${0})/settings.xml ]]
 then
-    mvn -s $(dirname ${0})/settings.xml -DskipTests --batch-mode clean package
+    mvn -s $(dirname ${0})/settings.xml -DskipTests --batch-mode -f ${WORKING_DIR} clean package
 else
-    mvn -DskipTests --batch-mode clean package
+    mvn -DskipTests --batch-mode -f ${WORKING_DIR} clean package
 fi
