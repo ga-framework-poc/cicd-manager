06e8df6c0d197f1b0ab7e86d6be0d550d803db02
Evan A Slatis <eslatis@redhat.com>
2022-05-11 14:26:17 -0500

    programming error

--- run-step/steps/infra/set-service-account-token.sh
+++ run-step/steps/infra/set-service-account-token.sh
@@ -16,8 +16,8 @@ do
 
     SERVICE_ACCOUNT_NAME=${SYSTEM_NAME}-${TEAM_NAME}-service-account
     SA_SECRET_NAME=$(oc get secrets -o custom-columns=:.metadata.name | grep -m 1 ${SERVICE_ACCOUNT_NAME}-token)
-    SA_TOKEN="::add-mask::{$(oc get secrets ${SA_SECRET_NAME} -o custom-columns=:.data.token | tr -d '[:space:]')}"
-    SA_TOKEN_DECODE="::add-mask::{$(echo ${SA_TOKEN} | base64 -s)}"
+    SA_TOKEN="$(oc get secrets ${SA_SECRET_NAME} -o custom-columns=:.data.token | tr -d '[:space:]')"
+    SA_TOKEN_DECODE="$(echo ${SA_TOKEN} | base64 -d)"
 
     oc login --token=${SA_TOKEN_DECODE} ${OCP_URL}
 
