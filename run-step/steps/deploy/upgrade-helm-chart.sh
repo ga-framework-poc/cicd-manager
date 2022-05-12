eef3114bd5956505f7d321a970e59c745cf254f1
Evan A Slatis <eslatis@redhat.com>
2022-05-11 21:22:07 -0500

    programming error

--- run-step/steps/deploy/upgrade-helm-chart.sh
+++ run-step/steps/deploy/upgrade-helm-chart.sh
@@ -4,20 +4,14 @@ set -ex
 
 oc login --insecure-skip-tls-verify --token ${OCP_SA_TOKEN} ${OCP_URL}
 
-echo
-ls ${WORKING_DIR}
-echo
-ls ${HELM_CHART_DIR}
-echo
-
 VALUES_FILE=$(find ${HELM_CHART_DIR} -name "values.y*ml")
-if [[ -f ${VALUES_FILE} ]]
+if [[ -f "${VALUES_FILE}" ]]
 then
     DEPLOY_VALUES_FILE="--values ${VALUES_FILE}"
 fi
 
 ENV_VALUES_FILE=$(find ${HELM_CHART_DIR} -name "values-${DEPLOY_ENV}.y*ml")
-if [[ -f ${ENV_VALUES_FILE} ]]
+if [[ -f "${ENV_VALUES_FILE}" ]]
 then
     DEPLOY_ENV_VALUES_FILE="--values ${ENV_VALUES_FILE}"
 fi
